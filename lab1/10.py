3c69ed84d56454bdd3f038a26b1aca47bca311b8
Kajetan Jankowski <kajetan.jankowski@interia.pl>
2021-10-05 18:24:16 +0000

    Update 10.py

--- lab1/10.py
+++ lab1/10.py
@@ -1,9 +1,18 @@
-a = 0
-b = 2
-c = 3
-if a ==0 and b == 0:
-    print("brak miejsc zerowych")
-elif a == 0 and b!=0:
+a = 1
+b = 6
+c = 9
+delta = b**2 - 4*a*c
+if a == 0 and b!=0:
     x = b/c
-    print("jedno miejsce zerowe", x,0)
-elif a!=0 and b!=0;
+    print("jedno miejsce zerowe\n",x," ",0)
+elif delta > 0:
+    print("2 miejsca zerowe")
+elif delta == 0:
+    x = (-b)/(2*a)
+    print("jedno miejsce zerowe\n", x, " ", 0)
+elif delta < 0:
+    print("brak miejsca zerowego")
+
+
+
+
