b9b16a262ffeec4f03737e4ecf36f00607cf3220
Kajojan <kojoty1999@gmail.com>
2021-10-05 18:40:15 +0000

    zmiana

--- lab1/8.py
+++ lab1/8.py
@@ -1,18 +1,30 @@
-a = 12311
-b = 1221
-c = 1101
-if a > b:
-    if c > a:
-        print(c , a , b)
+a = 1103
+b = 1101
+c = 1102
+#a
+if a > b and a > c:
+    if c == b:
+        print("c= ",c ,"b= ", b,"a= " ,a ,"\nc=b<a")
+    elif c > b:
+        print("a= " ,a ,"c= ",c ,"b= ", b, "\na>c>b")
     else:
-        print(a,c,b)
-elif b > a:
-    if c > b:
-        print(c , b , a)
+        print("a= " ,a,"c= ",c,"b= ",b, "\nc=b<a")
+#b
+elif b > a and b > c:
+    if a == c:
+        print("c= ",c ,  "a= " ,a,"b= ",b ,"\nc=a<b")
+    elif c > a:
+        print("b= ",b,"c= ",c,"a= " ,a, "\nb>c>a")
     else:
-        print(b,c,a)
-elif b > c:
-    if a > b:
-        print(a , b, c)
+        print("b= ",b,"a= " ,a,"c= ",c,"\nb>a>c")
+#c
+elif c > b and c > a:
+    if a == b:
+        print("c= ",c,"a= " ,a ,"b= ", b, "\na=b<c")
+    elif b > a:
+        print("c= ",c,"b= ",b,"a= " ,a, "\nc>b>a")
     else:
-        print(b,a,c)
+        print("c= ",c,"a= " ,a,"b= ",b,"\nc>a>b")
+
+elif a == c and c==b:
+        print("a=c=b")
