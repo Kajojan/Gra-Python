28a685f3bc8c782eb065108b1a90d5f87fa96ecb
Kajojan <kojoty1999@gmail.com>
2021-11-09 23:21:19 +0100

    zmiana

--- lab5/5.3.py
+++ lab5/5.3.py
@@ -1,42 +1,1 @@
-import random
-def randomlista(x):
-    lista=[]
-    l=0
-    for i in range(0,x):
-        x = random.randrange(l,100)
-        l=x
-        lista.append(x)
-    return lista
-lista1=randomlista(3)
-lista2=randomlista(3)
-dl=len(lista2)+len(lista1)
-print(lista1)
-print(lista2)
-k=0
-j=0
-lista3=[]
-for i in range (0,dl):
-    if k<len(lista1):
-        if lista1[k]<lista2[j]:
-            lista3.append(lista1[k])
-            if k<len(lista1)-1:
-                k = k + 1
-    elif j<len(lista2):
-        if lista1[k]>=lista2[j]:
-            lista3.append(lista2[j])
-            if j<len(lista2)-1:
-                j= j + 1
-    elif k == len(lista1) - 1:
-        lista3.append(lista2[j])
-        j = j + 1
-
-    elif j == len(lista2) - 1:
-        lista3.append(lista1[k])
-        k = k + 1
-
-
-
-print(lista3)
-
-
-
+import randomdef randomlista(x):    lista = []    l = 0    for i in range(0, x):        x = random.randrange(l, 100)        l = x        lista.append(x)    return listalista1 = randomlista(3)lista2 = randomlista(3)dl = len(lista2) + len(lista1)print(lista1)print(lista2)k = 0j = 0lista3 = []for i in range(0, dl ):    if k < len(lista1) and j < len(lista2):        if lista1[k] <= lista2[j]:            lista3.append(lista1[k])            k = k + 1        else:            lista3.append(lista2[j])            j = j + 1    elif k == len(lista1):        lista3.append(lista2[j])        j = j + 1    elif j == len(lista2):        lista3.append(lista1[k])        k = k + 1print(lista3)
