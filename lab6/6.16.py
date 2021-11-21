a03c09f5ff42748d40cf0ccb526bd84720fdef71
Kajojan <kajetan.jankowski@interia.pl>
2021-11-21 19:39:00 +0100

    zad6

--- lab6/6.16.py
+++ lab6/6.16.py
@@ -6,5 +6,25 @@ def czy_ddatnie(a,b,c):
 
 
 def czy_trojkat(a,b,c):
-    if czy_ddatnie(a,b,c)= "True":
-        if
+    if czy_ddatnie(a,b,c)==True:
+        if a>b and a>c:
+            if b+c>a:
+                return True
+            else:
+                return False
+        elif b>a and b>c:
+            if a+c>b:
+                return True
+            else:
+                return False
+        elif c>b and c>a:
+            if b+a>c:
+                return True
+            else:
+                return False
+
+    else:
+        return False
+
+
+print(czy_trojkat(3, 5, 8))
