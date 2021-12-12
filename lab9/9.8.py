f92421fbf18d7e0c605640c3dc6b1fc78e4c0c3b
Kajojan <kojoty1999@gmail.com>
2021-12-13 00:01:01 +0100

    zmiana

---
+++ lab9/9.8.py
@@ -0,0 +1,4 @@
+modele={"Audi":"rs7", "Audi":"a3", "Toyota":"Yaris", "Toyota":"Auris"}
+rok_produkcji={"Audi":"2017", "Audi":"2002", "Toyota":"2004", "Toyota":"2010"}
+print(modele)
+print(rok_produkcji)
