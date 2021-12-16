880e3660a07ed028e65063de0171982ef8134db7
Kajetan Jankowski <kjankowski@inf.ug.edu.pl>
2021-12-16 10:06:24 +0100

    zad

---
+++ lab10/assertTEST3.py
@@ -0,0 +1,20 @@
+def sort(lista):
+    n=len(lista)
+    print(lista)
+    sorted_list=lista
+    i=0
+
+    while i < n:
+        k = 0
+        while k < n-1:
+            if sorted_list[k] > sorted_list[k+1]:
+                p=sorted_list[k]
+                sorted_list[k]=sorted_list[k+1]
+                sorted_list[k+1]=p
+            k+=1
+        i+=1
+    return sorted_list
+
+def test_sort():
+    lista = [1, 2, 3, 4, 4, 1, 2, 9, 8, 7, 6]
+    assert sort(lista)==sorted(lista)
---
+++ lab10/assertTEST4.py
@@ -0,0 +1,7 @@
+def suma(a,b):
+    suma=a+b
+    return suma
+def test_suma():
+    a=2
+    b=2
+    assert suma(a,b)==a+b
