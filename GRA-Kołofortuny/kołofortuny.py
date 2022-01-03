f86578a28b781bd63dd6c8d3b444c0ce732e8deb
kjankowski <kajetan.jankowski2interia.pl>
2022-01-03 17:50:19 +0100

    Changes

---
+++ GRA-Kołofortuny/kołofortuny.py
@@ -0,0 +1,25 @@
+import random
+def kolo():
+    kolo1={1:100, 2:250, 3:500, 4:550, 5:800, 6:1000, 7:3000, 8:"500?", 9:"Nagroda", 10:"bankrut"}
+    x = random.randrange(10)
+    for i in kolo1.keys():
+        if i == x:
+            return kolo1[i]
+
+
+
+
+
+print("Witamy w kole fortuny!!!")
+print("Przedstawmy naszego gościa")
+imie=input("Jak sie nazywasz?")
+print("Witaj", imie)
+print("Już tłumacze ci zasady")
+print("Na kole znajdują się: pieniadze, nagroda, bankrut, albo 500?. Jesli to wulosujesz stoisz przed wyborem, albo  500 zł albo odkrywasz karte i masz 50/50 na bankruta albo 3500zł ")
+print("Po zakręceniu kołem podajesz spółgłoske, tyle ile bedzie ich w haśle, tyle razy dostaniesz wylosowane pieniadze")
+print("Jeśli nie zgadniesz, lub wylosujesz bankryta  przegrywasz wszystko")
+print("W każdj chwili możesz zgadywać hasło, pamietaj aby być bardzo dokładnym w podawaniu hasła ")
+print("Zaczynamy")
+start=input("Tak czy Nie ?")
+if start=="Tak":
+    print("losowanie hasła.....")
